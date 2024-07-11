33b50eb93f3a9e7d9c0a1c4309753020f5e71577
Azozz5 <161593795+Azozz5@users.noreply.github.com>
2024-07-11 21:24:00 +0300

    net1.js

--- net1.js
+++ net1.js
@@ -1,1 +1,9 @@
 
+$(`<center><div id="sultan13"><a href="https://up6.cc/2024/07/172003934841941.jpg" style="width:90px;margin:2px; border-radius: 0px 10px 0px 10px;" class="fl  fa   fa-phone btn btn-warning"> الإدارة </a>
+<a href="https://up6.cc/2024/07/171978546264352.jpg" style="width:90px;margin:2px; border-radius: 0px 10px 0px 10px;" class="fl  fa  fa-heartbeat
+ btn btn-primary"> الادمن </a>
+<a href="https://up6.cc/2024/07/171978546266843.jpg" style="width:90px;margin:2px; border-radius: 0px 10px 0px 10px;" class="fl  fa   fa-star
+ btn btn-success"> السوابر </a>
+<a href="https://up6.cc/2024/07/171987615645661.gif" style="width:90px;margin:2px; border-radius: 0px 10px 0px 10px;" class="fl  fa fa-graduation-cap
+btn btn-info"> القوانين </a>
+`).insertBefore('.nav.nav-tabs');
