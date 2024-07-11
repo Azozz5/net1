6e5db446d9a1828617de8ee66ec025d06f042fb1
Azozz5 <161593795+Azozz5@users.noreply.github.com>
2024-07-11 22:27:56 +0300

    net1.js

--- net1.js
+++ net1.js
@@ -1,72 +1,125 @@
-$(`<center><div id="sultan13"><a href="https://up6.cc/2024/07/172003934841941.jpg" style="width:90px;margin:2px; border-radius: 0px 10px 0px 10px;" class="fl  fa   fa-phone btn btn-warning"> الإدارة </a>
-<a href="https://up6.cc/2024/07/171978546264352.jpg" style="width:90px;margin:2px; border-radius: 0px 10px 0px 10px;" class="fl  fa  fa-heartbeat
- btn btn-primary"> الادمن </a>
-<a href="https://up6.cc/2024/07/171978546266843.jpg" style="width:90px;margin:2px; border-radius: 0px 10px 0px 10px;" class="fl  fa   fa-star
- btn btn-success"> السوابر </a>
-<a href="https://up6.cc/2024/07/171987615645661.gif" style="width:90px;margin:2px; border-radius: 0px 10px 0px 10px;" class="fl  fa fa-graduation-cap
-btn btn-info"> القوانين </a>
-`).insertBefore('.nav.nav-tabs');
-$(`<style>
-#sultan {color: #f00!important;}
-
-
-span.btn.fr.fa.fa-eye {transform: matrix(1,0,0,1,-40,-3);}
-.fa.fa-user.label.badgex.label-success {transform: matrix(1,0,0,1,-55,0);}
-div#l1 input#u1,div#l3 button{width:98%!important;margin-left:3px}
-div#l2 button,div#l3 button{margin-left: 3px;border:1px solid #ccc!important;border-radius:0px 10px 0px 10px!important;height:27px!important;padding:0 5px;}
-div#l1 input#u1{height: 25px!important;border-radius:2px;border:1px solid #e1e0e0;margin-top:3px!important;text-align:center}
-div#l2 input#pass1,div#l2 input#u2,div#l3 input#pass2,div#l3 input#u3{border-radius:0px 5px 5px 5px!important;
-box-shadow: 0 0 0px rgb(0 0 0), inset 0 0 2px rgb(0 0 0), 0 0 0 0px #000;height: 23px!important;width:49%!important;padding:0!important;float:right;margin:2px 5px 2px 0}
-div#l2 input#u2,div#l3 input#u3{margin-left:-6px;border:1px solid #e1e0e0;text-align:center}
-
-div#l2 input#pass1,div#l3 input#pass2{border-radius:15px;border:1px solid #e1e0e0;text-align:center}
-div#l2,div#l3{padding:4px!important}
-div#l1{margin:0 1px -18px;padding: 4px!important;}
-div#l1,div#l2,div#l3{border-bottom:2px solid;margin-bottom:2px;border-top:2px solid;margin-top:2px;border-color:#bababa;border-radius:0px 10px 0px 10px!important;box-shadow: 0 0 0px rgb(0 0 0), inset 0 0 1px rgb(0 0 0), 0 0 0 0px #000;height:height:69px}
-div#l1 button{border:1px solid #ccc!important;border-radius:2px!important;width:98%;height:27px;margin-left: 3px;padding:0 5px}
-div#l2 button{width:70%}
-
-ul.nav.nav-tabs>li>a{border:none;text-align:center;border-radius:3px;display:initial;width:80%;float:left;padding:0!important}
-
-ul.nav.nav-tabs>li.active>a{border:none;border-radius:3px}ul.nav.nav-tabs>li{width:32%;margin:0 5px 0 0}
-ul.nav.nav-tabs>li>.fa-user-plus:before,ul.nav.nav-tabs>li>.fa-user:before,ul.nav.nav-tabs>li>.fa-user:before{float:right;margin-right:-26%;padding:0 2px 1px;border-radius:3px;width:23px;text-align:center}
-.nav>li>a:focus,.nav>li>a:hover{color:#9abff9}
-ul.nav.nav-tabs{margin-top:2px;float:right;width:100%;padding:3px 1px}
-
-div#tlogins::-webkit-scrollbar{width:0px;height:10px}
-
-div#tlogins .fl.mini.u-msg{margin-top:3px!important;border-radius:3px!important}
-a.label.label-primary.fl img.fl,button.btn.fr.btn-success.fa.fa-refresh,div#l2 br,div#l3 br{display:none}
-
-
-a.label.label-primary.fl {
-    border-radius: 3px!important;
-    margin: 2px -155px;
-    padding: 2px!important;
-    margin-bottom: 2px;
-    text-align: right!important;
+$("style").last().append(`
+#users .uzr.azoz1 img.co {
+  width: 0px !important;
+    height: 0px !important;
+  margin-right: 0px;
 }
 
-button.btn.fr.btn-success.fa.fa-refresh{display:none}
-a.label.label-primary.fl img.fl{display:none}
-#des22{text-align:center!important;font-size:100%!important}
-.fr.borderg,.fr.borderg a{font-size:10px!important;padding:0 5px!important;position:relative;color:#ffffff}
-.fr.borderg{border:0;margin-top:0;height:0}
-span.s1.fa.fa-user.label.badgex.label-as-badge.label-success{padding:0 3px 2px;margin-right:5px}
-label.label.label-primary.mini.fl{padding:1px;border-radius:3px!important}
+#users .uzr.azoz1 span.uhash{
+    -webkit-text-fill-color: transparent;
+width: 0px !important;
+    height: 0px !important;
+  margin-right: 0px;}
 
 
-span.btn.fr.btn-success.fa.fa-refresh {display: none;}
-.des{position:absolute;transform:matrix(1,0,0,1,86,-197);margin-top:2px;width:300px;height:25px;float:left;background:transparent url(https://www.te3b.com/sico/z1e1qdfn1f10.png) no-repeat scroll right top;text-align:left}
-div#tlogins::-webkit-scrollbar{width:0;height:10px}
-div#tlogins .fl.mini.u-msg{margin-top:3px!important;border-radius:3px!important}
-a.label.label-primary.fl img.fl,button.btn.fr.btn-success.fa.fa-refresh,div#l2 br,div#l3 br{display:none}
-a.label.label-primary.fl{height: 26px;border-radius:3px!important;margin:2px -155px;padding:2px!important;margin-bottom:2px;text-align:right!important}button.btn.fr.btn-success.fa.fa-refresh{display:none}
-a.label.label-primary.fl img.fl{display:none}
-#des22{text-align:center!important;font-size:100%!important}
-.fr.borderg,.fr.borderg a{font-size:10px!important;padding:0 5px!important;position:relative;color:#fff}
-.fr.borderg{border:0;margin-top:0;height:0}span.s1.fa.fa-user.label.badgex.label-as-badge.label-success{padding:0 3px 2px;margin-right:5px}
-label.label.label-primary.mini.fl{padding:1px;border-radius:3px!important}
-</style>`).insertBefore("body")
+#users .uzr.azoz1 img.u-ico {
+margin-left: 3px;
 
-$.getScript("https://raw.githubusercontent.com/Azozz5/user1/main/user.js");
+     
+
+      
+    }
+
+#users .uzr.azoz1 img.ustat {
+  width: 13px !important;
+    height: 13px !important;
+    min-height: 0%!important;
+    margin-right: 0;
+    z-index: 999999;
+    border-radius: 0px 5px 0px 5px;
+    margin-left: 1px;
+    margin-top: 1px;
+    border: 1px solid #fff;
+    box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 2px #f0c01f;
+box-shadow: 0 0 3px rgb(0, 0, 0), inset 0 0 4px rgb(0, 0, 0), 0 0 0 0px #000;
+      
+      
+    }
+
+#users .uzr.azoz1 .fitimg.u-pic {
+
+  
+
+
+  z-index: 0 !important;
+
+height: 52px !important;
+
+ 
+margin-bottom: 4px !important;
+margin-top: 3px !important;
+border-radius: 100px;
+margin-left: -10px;
+
+padding: 26px !important;
+border:1px solid #fbfcfc;margin-top:1px;
+
+box-shadow:inset 0 0 0 rgba(0,0,0,.08),0 0 3px #000
+filter: hue-rotate(360deg);
+box-shadow: 0 0 3px rgb(0 0 0), inset 0 0 4px rgb(0 0 0), 0 0 0 0px #000;
+}
+
+#users .uzr.azoz1 .u-msg {
+margin-top: 2px;
+   -webkit-text-fill-color: #000;
+margin-left: 5px;
+
+  font-size: 89% !important;
+
+}
+
+
+
+
+
+#users .uzr.azoz1 {
+   padding: 0px !important;
+  box-shadow: inset 0 0 0 rgba(0,0,0,.08),0 0 2px #000;
+  margin-bottom: 3px !important;
+  margin-top: 2px !important;
+   border-radius: 0px 10px 0px 10px;
+   
+  
+  border: 1px solid #fff;
+ background-image: url(https://up6.cc/2024/01/170622859220941.png);
+background-size: 100%;
+
+    
+}
+
+
+
+#users .uzr.azoz1 .d-flex.fl {
+  padding-right: 0px !important;
+}
+
+#users .uzr.azoz1 .u-topic {
+margin-left: 200px;
+   -webkit-text-fill-color: #0000!important;
+
+}
+
+#users .uzr.azoz1 .itarr_azoz1 {
+ width: 0px;
+
+margin-top: -5px;
+cursor: pointer;
+margin-left: 9px;
+
+
+}
+`);
+var l_itvnour=setInterval(function(){
+    if(myid!=null){
+        clearInterval(l_itvnour);
+        $("#users .uzr:contains('azoz')").addClass('azoz1');
+        $("div#users .uzr.azoz1 .My_vedio1").click(function(){
+        $("#movon1").show();
+        $('#movon1').addClass('raken1');
+        
+        $("#ifr_mov1").show(function(){
+        $("#upro").modal("hide");
+    } ); 
+});
+    }
+}, 10000);
